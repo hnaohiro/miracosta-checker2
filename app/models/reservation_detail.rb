ebbcdea31f576623cce41a875256c674e75a3bcc
hnaohiro <hasegawanaohiro1989@gmail.com>
2018-04-15 19:12:50 +0900

    reservable_link

--- app/models/reservation_detail.rb
+++ app/models/reservation_detail.rb
@@ -1,4 +1,8 @@
 class ReservationDetail < ApplicationRecord
   belongs_to :room
   belongs_to :target_date
+
+  def reservable_link
+    "https://reserve.tokyodisneyresort.jp/hotel/list/?roomsNum=1&adultNum=2&childNum=0&stayingDays=1&useDate=#{target_date.date_for_query}&searchHotelCD=DHM&hotelSearchDetail=true&displayType=hotel-search"
+  end
 end
